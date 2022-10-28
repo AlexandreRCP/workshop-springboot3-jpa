49855c656648324bcc1f62886ce01ce08f1a7ccf
AlexandreRCP <alex.rcp@hotmail.com>
2022-10-28 14:29:32 -0300

    Category

---
+++ src/main/java/com/developeweb/developer/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.developeweb.developer.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.developeweb.developer.entities.Category;
+import com.developeweb.developer.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
+
